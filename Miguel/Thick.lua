4e332656a0366cc93f89a6fa63ce53c46bd9f2ed
Star <125512934+StaryLOL@users.noreply.github.com>
2023-07-17 16:46:29 -0700

    Create Thick.lua

---
+++ Miguel/Thick.lua
@@ -0,0 +1,6 @@
+ game.Lighting.Sky.SkyboxBk = "http://www.roblox.com/asset/?id=14087817155"
+                game.Lighting.Sky.SkyboxDn = "http://www.roblox.com/asset/?id=14087794328"
+                game.Lighting.Sky.SkyboxFt = "http://www.roblox.com/asset/?id=14087841309"
+                game.Lighting.Sky.SkyboxLf = "http://www.roblox.com/asset/?id=14087823464"
+                game.Lighting.Sky.SkyboxRt = "http://www.roblox.com/asset/?id=14087833790"
+                game.Lighting.Sky.SkyboxUp = "http://www.roblox.com/asset/?id=14087848589"
