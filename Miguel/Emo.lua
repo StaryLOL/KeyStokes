dd88184faa96c2d694c48dfaf2fcb888d62f40ba
Star <125512934+StaryLOL@users.noreply.github.com>
2023-07-17 19:43:20 -0700

    Create Emo.lua

---
+++ Miguel/Emo.lua
@@ -0,0 +1,6 @@
+game.Lighting.Sky.SkyboxBk = "http://www.roblox.com/asset/?id=14090457010"
+                game.Lighting.Sky.SkyboxDn = "http://www.roblox.com/asset/?id=14090502186"
+                game.Lighting.Sky.SkyboxFt = "http://www.roblox.com/asset/?id=14090477471"
+                game.Lighting.Sky.SkyboxLf = "http://www.roblox.com/asset/?id=14090482242"
+                game.Lighting.Sky.SkyboxRt = "http://www.roblox.com/asset/?id=14090464849"
+                game.Lighting.Sky.SkyboxUp = "http://www.roblox.com/asset/?id=14090498828"
